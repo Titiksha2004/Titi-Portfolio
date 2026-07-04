b114ec517ced6bdca0ab294e758be4ee23704a1d
Titiksha <kamanatitiksha@gmail.com>
2026-07-04 10:22:18 +0530

    COnfigured change for pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,16 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/Titi-Portfolio/',
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Titi-Portfolio/',
 })
